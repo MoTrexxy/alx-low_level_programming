cd728818838184367813f22cd5f1f0d0c676a3b8
Odeyemi Motunrayo <odeyemimotunrayo9@gmail.com>
2023-03-20 03:21:24 -0700

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+/**
+* main - print all possible different combinations of two digits
+* Author: Odeyemi Motunrayo
+* Return: 0
+*/
+int main(void)
+{int inum1 = 0;
+int num2;
+while (num1 < 9)
+{num2 = num1 + 1;
+while (num2 < 10)
+{putchar('0' + num1);
+putchar('0' + num2);
+if (num1 == 8)
+{putchar('\n');
+break;
+}
+putchar(',');
+putchar(' ');
+num2++;
+}
+num1++;
+}
+return (0);
+}
