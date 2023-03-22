ef18ce929d1bfad84ed59d3445acd0f88c466a37
Odeyemi Motunrayo <odeyemimotunrayo9@gmail.com>
2023-03-21 19:40:49 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,11 +5,10 @@
 * Return: always 0;
 */
 
-void print_alphabet(void);
-{
-	char i
-		;
-	for (i = 'a' ; i <= 'z' ; i++)
+void print_alphabet(void)
+{ 
+	char i;
+	for (i = 'a'; i <= 'z'; i++)
 		_putchar(i);
 	_putchar('\n');
 }
