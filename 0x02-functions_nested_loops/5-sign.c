6d259754986ed79a7d8f646b4899fdb4de8fbdb8
Odeyemi Motunrayo <odeyemimotunrayo9@gmail.com>
2023-03-22 07:30:48 -0700

    5-sign.c file added

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
 * print_sign - return 0 letter not lowercase, 1 letter lowercase
 *
@@ -7,21 +8,22 @@
 */
 int print_sign(int n)
 {
-         int test;
-         if (n > 0)
-         {
-                 test = 1;
-                 _putchar('+');
-         }
-         else if (n == 0)
-         {
-                 test = 0;
-                 _putchar('0');
-         }
-         else
-         {
-                 test = -1;
-                 _putchar('-');
-         }
-         return (test);
+	int test;
+
+	if (n > 0)
+	{
+		test = 1;
+		_putchar('+');
+	}
+	else if (n == 0)
+	{
+		test = 0;
+		_putchar('0');
+	}
+	else
+	{
+		test = -1;
+		_putchar('_');
+	}
+	return (test);
 }
