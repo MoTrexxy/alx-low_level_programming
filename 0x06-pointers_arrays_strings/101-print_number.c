306b85fa5b2ed91579da815c019dc0e022d505da
Odeyemi Motunrayo <odeyemimotunrayo9@gmail.com>
2023-03-30 05:09:06 -0700

    102-magic.c

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+* print_number - prints integers enters as parameters using putchar
+* @n: integer to print
+* Return: void
+*/
+void print_number(int n)
+{
+	int power;
+
+	power = base10(8);
+
+	if (n < 0)
+	{
+		_putchar('-');
+		n *= -1;
+	}
+
+	if (n == 0)
+		_putchar('0');
+
+	else
+	{
+		while (n / power == 0)
+			power /= 10;
+
+		while (power >= 1)
+		{
+			_putchar((n / power) + '0');
+			n %= power;
+			power /= 10;
+		}
+	}
+}
