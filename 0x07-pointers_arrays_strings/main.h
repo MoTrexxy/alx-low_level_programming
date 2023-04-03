69f7b730fcb07ce57568f1df731bb99ba2e9bc9b
Odeyemi Motunrayo <odeyemimotunrayo9@gmail.com>
2023-04-03 13:36:12 -0700

    1-memcpy.c file created

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -2,8 +2,6 @@
 #define _main_h_
 
 char *_memset(char *s, char b, unsigned int n);
-void simple_print_buffer(char *buffer, unsigned int size);
-int main(void);
 char *_memcpy(char *dest, char *src, unsigned int n);
 
 #endif
