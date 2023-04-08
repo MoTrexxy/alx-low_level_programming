80e4e72336c7df5d75283d305453c72ba089f577
Odeyemi Motunrayo <odeyemimotunrayo9@gmail.com>
2023-04-08 07:10:51 -0700

    1-args.c file created

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+* main - main function
+* @argc: argument count
+* @argv: argument function
+* Return: 0
+*/
+int main(int argc, char __attribute__((__unused__)) *argv[])
+{
+	printf("%d\n", argc - 1);
+
+	return (0);
+}
