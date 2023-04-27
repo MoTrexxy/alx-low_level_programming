a4896aa84a6f9ec4c09ea87a803cbca01b25dc69
MoTrexxy <odeyemimotunrayo9@gmail.com>
2023-04-27 11:28:29 -0700

    100-first.c file created

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 
-void first(void) __attribute__ ((constructor));
+void first(void) __attribute__ ((constructor)) ht(void);
 
 /**
- * function - that prints you're beat.. before the main funct is executed
- * Return: zero
+ * ht - funct that prints a string before the main funct is executed
  */
-void first(void)
+void ht(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
