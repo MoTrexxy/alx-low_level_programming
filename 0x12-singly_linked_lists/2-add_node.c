59eeafbd5c37aebe167024105f48ab176ccafa81
MoTrexxy <odeyemimotunrayo9@gmail.com>
2023-04-27 10:21:03 -0700

    2-add_node.c file created

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,29 +1,29 @@
-#include <stdlib.h>
-#include <string.h>
 #include "lists.h"
 
 /**
  * add_node - adds a new node at the beginning of a linked list
- * @head: double pointer to the liked list
- * @str: new string to add in the node
- * Return: the address of the new element, or NULL if it failed
+ * @head: head of the linked list
+ * @str: string to add in the node
+ * Return: address of the new element, or NULL if it failed
  */
+
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new;
-	unsigned int length = 0;
-
-	while (str[length])
-		length++;
+	size_t countchar;
 
 	new = malloc(sizeof(list_t));
-	if (!new)
+	if (new == NULL)
 		return (NULL);
 
-	new->str = strdup(str);
-	new->length = length;
-	new->next = (*head);
-	(*head) = new;
+	new->str = strcpy(str);
+
+	for (countchar = 0; str[countchar]; countchar++)
+		;
+
+	new->len = countchar;
+	new->next = *head;
+	*head = new;
 
 	return (*head);
 }
