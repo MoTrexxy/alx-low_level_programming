2b857acb2aca437534ecee0643b69511347d054b
MoTrexxy <odeyemimotunrayo9@gmail.com>
2023-05-04 13:47:48 -0700

    100-get_endianness.c file created

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * get_endianness - a funct that checks the endianness
+ * Return: 0 if big endian, 1 if little endian
+ */
+int get_endianness(void)
+{
+	unsigned int m = 1;
+	char *c = (char *) &m;
+
+	return (*c);
+}
